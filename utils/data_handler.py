e6a2d98912f352a0d93a312e68ee5c6a7c56ec86
gamboel99 <eko.w.a@students.kahuripan.ac.id>
2025-07-11 10:55:01 +0700

    Update data_handler.py

--- utils/data_handler.py
+++ utils/data_handler.py
@@ -1,1 +1,5 @@
-# Modul data_handler.py
+def save_data(file_path, new_data):
+    # Menyimpan data baru ke file CSV, otomatis menambahkan jika file sudah ada.
+
+def load_data(file_path):
+    # Membaca file CSV, atau mengembalikan DataFrame kosong jika belum ada.

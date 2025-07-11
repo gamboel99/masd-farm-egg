1fcf4b84cb094ea2408494b45f1168fb4ddd8a52
gamboel99 <eko.w.a@students.kahuripan.ac.id>
2025-07-11 11:43:37 +0700

    Update data_handler.py

--- utils/data_handler.py
+++ utils/data_handler.py
@@ -1,9 +1,19 @@
-import pandas as pd
-import os
+def export_to_excel(df_pakan, df_jual, biaya_pakan, pendapatan, laba):
+    import xlsxwriter
+    from io import BytesIO
 
-def save_data(filepath, data_dict):
-    df = pd.DataFrame([data_dict])
-    if os.path.exists(filepath):
-        df_existing = pd.read_csv(filepath)
-        df = pd.concat([df_existing, df], ignore_index=True)
-    df.to_csv(filepath, index=False)
+    output = BytesIO()
+    writer = pd.ExcelWriter(output, engine='xlsxwriter')
+
+    df_pakan.to_excel(writer, sheet_name="Pakan", index=False)
+    df_jual.to_excel(writer, sheet_name="Penjualan", index=False)
+
+    df_summary = pd.DataFrame({
+        "Keterangan": ["Pendapatan", "Biaya Pakan", "Laba Bersih"],
+        "Nilai (Rp)": [pendapatan, biaya_pakan, laba]
+    })
+    df_summary.to_excel(writer, sheet_name="Laba Rugi", index=False)
+
+    writer.close()
+    output.seek(0)
+    return output.getvalue()
